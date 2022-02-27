6bd46442fb429eb53d922bf2985a81ab77731076
milmaj <milad.majidi@me.com>
2022-02-27 09:18:32 +0100

    initialize sass

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,8 @@
-import '../styles/globals.css'
+import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
+import variables from '../styles/variables.module.scss'
+
+console.log(variables.primaryColor);
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
